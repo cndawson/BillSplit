d9fb663a5227adaaf8979cf1461d945e41be5050
christianstain <christianmancha@outlook.com>
2017-12-04 04:49:31 -0700

    Better CSS in login

--- BillSplit/index.php
+++ BillSplit/index.php
@@ -14,19 +14,16 @@ Author: Caylie Dawson & Christian Mancha
 	<div class="centered">
 		<img id="logo" src="images/whitelogo.png"/><br>
 		<form action="controller.php" method="POST">
-			Username <input class="fields" type="text" name="IDL"> <br> 
-			Password <input class="fields" type="password" name="passwordL"> <br> 
-			<input type="submit" name="Login" value="Login"> <br> <br>
-	<?php
-	
-	if (isset ( $_SESSION ['loginError'] ))
-		echo $_SESSION ['loginError'];
-	
-	?>
-</form>
-		<br>
-		<p>Don't already have an account?</p>
-		<a href="register.php"><button>Register</button></a><br>
+			<span class="font">Username</span> <input class="fields" type="text" name="IDL"> 
+			<span class="font">Password</span> <input class="fields" type="password" name="passwordL"> <br> 
+			<input class="buttonLogin" type="submit" name="Login" value="Login"> <br>
+		</form>
+		<?php
+			if (isset ( $_SESSION ['loginError'] ))
+				echo "<br><span class=\"font\">" . $_SESSION ['loginError'] . "<span/>";
+		?>
+		<p><span class="font">Don't already have an account?</span></p>
+		<a href="register.php"><button class="buttonLogin">Register</button></a><br>
 	</div>
 </body>
 </html>
--- BillSplit/register.php
+++ BillSplit/register.php
@@ -5,25 +5,25 @@ Author: Caylie Dawson
 <html>
 <head>
 	<title>Register Page</title>
-	<link rel="stylesheet" type="text/css" href="styleRegister.css"/>
+	<link rel="stylesheet" type="text/css" href="styleLogin.css"/>
 	<?php
 		session_start ();
 	?>
 </head>
 <body>
 <div class="centered">
-<h3>Register</h3>
+<h3><span class="font">Sign up</span></h3>
 	<form action="controller.php" method="POST">
-		Username <input type="text" class="fields" name="ID" id="ID" pattern=".{4,}" required> <br>
-		Password <input type="password" class="fields" name="password" id="password" pattern=".{6,}"  required> <br>
+		<span class="font">Username</span> <input type="text" class="fields" name="ID" id="ID" pattern=".{4,}" required>
+		<span class="font">Password</span> <input type="password" class="fields" name="password" id="password" pattern=".{6,}"  required> <br>
 		<!-- Email <input type="text" name="email" id="email" required> <br> -->
-	<input type="submit" name="Register" value="Register"> <br> <br>
+	<input class="buttonLogin" type="submit" name="Register" value="Register"> <br>
 	<?php
 	//$pwd = document.getElementById(password).value;
 	//$hash = password_hash($pwd, PASSWORD_DEFAULT);
 	//echo $hash;
   if( isset(  $_SESSION['registerError']))
-    echo   $_SESSION['registerError'];
+  	echo  "<span class=\"font\">" . $_SESSION['registerError'] . "<span/>";
 	
 	?>
 </form>
