b992407e545efb91d62f418bb30243b62a6228cd
Caylie Dawson <cnd@email.arizona.edu>
2017-12-06 15:05:16 -0800

    -settings css commit

---
+++ BillSplit/settings.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!-- 
+Author: Caylie Dawson & Christian Mancha
+-->
+<html>
+<head>
+<title>Settings</title>
+<link rel="stylesheet" type="text/css" href="styleSettings.css" />
+	<?php
+	session_start ();
+	?>
+</head>
+<body>
+<div id="mainContainer">
+<div id="header">
+		<img id="logo" src="images/blacklogo.png"/>
+		<a href="main.php"><button id="settingsButton" type="submit">Home</button></a>
+	</div>
+	<div id="centered">
+	Change Profile Picture<br>
+	<button id="edit" type="submit">Edit</button><br><br>
+	Chose Payment Method
+
+	<form id="paymentMethod" action="controller.php" method="POST">
+  <input type="radio"  value="Credit" id="credit" name="paymentType"> Credit<br>
+  <input type="radio"  value="Debit" id="debit" name="paymentType"> Debit<br>
+  <input type="radio"  value="Paypal" id="paypal" name="paymentType"> Paypal<br>
+  <button class="buttonAdd" type="submit">Confirm Payment</button>
+</form>
+	</div>
+	</div>
+	</body>
+	</html>
